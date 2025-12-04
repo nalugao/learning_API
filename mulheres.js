2886b29a5e8a812cb4b9711741d7cbc2128c1525
nalugao <nalugao@hotmail.com>
2025-12-04 13:17:46 -0300

    feat: add package.json and package-lock.json; initialize mulheres.js with sample data and GET route

--- mulheres.js
+++ mulheres.js
@@ -1,36 +1,57 @@
-const express = require("express");
-const router = express.Router();
+// const { v4: uuidv4 } = require("uuid");
 
-const app = express();
-const porta = 3333;
+const express = require("express"); //inicio do express
+const router = express.Router(); //configuração da primeira parta da rota
 
+const app = express(); //iniciando o App
+const porta = 3333; //criandoa porta
+
+//abaixo, criação da lista inicial de mulheres
 const listaMulheres = [
   {
+    id: "1",
     nome: "Natalia Lugão",
     imagem: "https://avatars.githubusercontent.com/u/188492757?v=4",
     minibio: "Desenvolvedora em formação",
   },
   {
+    id: "2",
     nome: "Natalia Lugão",
     imagem: "https://avatars.githubusercontent.com/u/188492757?v=4",
     minibio: "Desenvolvedora em formação",
   },
   {
+    id: "3",
     nome: "Natalia Lugão",
     imagem: "https://avatars.githubusercontent.com/u/188492757?v=4",
     minibio: "Desenvolvedora em formação",
   },
 ];
 
+//abaixo GET
 function mostraMulheres(request, response) {
   response.json(listaMulheres);
 }
 
+//abaixo POST
+// function criaMulher(request, response) {
+//   const novaMulher = {
+//     id: uuidv4(),
+//     nome: request.body.nome,
+//     imagem: request.body.imagem,
+//     minibio: request.body.minibio,
+//   };
+
+//   listaMulheres.push(novaMulher);
+
+//   response.json(listaMulheres);
+// }
+
+//Função Porta
 function mostraPorta() {
-  console.log(
-    `O servidor está sendo transmitido em http://localhost:${3333}/mulheres`
-  );
+  console.log("O servidor está sendo transmitido na porta, ", porta);
 }
 
-app.use(router.get("/mulheres", mostraMulheres));
-app.listen(porta, mostraPorta);
+app.use(router.get("/mulheres", mostraMulheres)); //configuração da rota GET /mulheres
+// app.use(router.post("/mulheres", criaMulher)); //configuração de rota POST /mulheres
+app.listen(porta, mostraPorta); // servidor ouvindoa  porta
