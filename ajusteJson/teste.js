bc94b1363bfe4994757cb53f9cdcedf2abf873cc
nalugao <nalugao@hotmail.com>
2025-12-05 00:17:03 -0300

    ajuste de formatação

--- ajusteJson/teste.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const formatarJSON = require('./formatador.js');
-
-const textoBaguncado = fs.readFileSync('./entrada.json', 'utf8');
-
-const resultado = formatarJSON(textoBaguncado);
-
-console.log(resultado);
